d042790e47878a2afad8c6090c441b8f907b501d
vladimirStary <puffychords@gmail.com>
2023-11-29 23:56:15 +0300

    test 2

--- src/components/main-screen/message.buble.tsx
+++ src/components/main-screen/message.buble.tsx
@@ -1,6 +1,20 @@
-const MessageBubble = () => {
+import { Chip } from "@nextui-org/react";
+import { FC } from "react";
+
+interface MessageBubbleProps {
+    isMine: boolean;
+    text: string;
+}
+
+const MessageBubble: FC<MessageBubbleProps> = (props) => {
     return (
-        <></>
+        <>
+            <Chip
+                color={props.isMine ? 'primary' : 'secondary'}
+            >
+                Test message
+            </Chip>
+        </>
     );
 }
 
